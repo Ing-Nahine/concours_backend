47ca468af26d2809bfaa929723efdac4f3636910
Ing-Nahine <combarinahine@gmail.com>
2025-10-22 08:18:59 +0100

    add superuser

--- couldiat_project/accounts/management/commands/create_initial_superuser.py
+++ couldiat_project/accounts/management/commands/create_initial_superuser.py
@@ -3,22 +3,23 @@ from accounts.models import User
 import os
 
 class Command(BaseCommand):
-    help = 'Crée le superuser initial si inexistant'
+    help = 'Crée ou met à jour le superuser initial'
 
     def handle(self, *args, **kwargs):
-        # Récupérer les infos depuis les variables d'environnement (Render)
         email = os.environ.get('DJANGO_SUPERUSER_EMAIL', 'combarinahine@gmail.com')
         nom = os.environ.get('DJANGO_SUPERUSER_NOM', 'GBABLI MATHIEU')
         prenom = os.environ.get('DJANGO_SUPERUSER_PRENOM', 'Nahine')
         password = os.environ.get('DJANGO_SUPERUSER_PASSWORD', 'BARIcom0622')
 
-        if not User.objects.filter(email=email).exists():
-            User.objects.create_superuser(
-                email=email,
-                nom=nom,
-                prenom=prenom,
-                password=password
-            )
+        user, created = User.objects.get_or_create(email=email)
+        user.nom = nom
+        user.prenom = prenom
+        user.is_staff = True
+        user.is_superuser = True
+        user.set_password(password)
+        user.save()
+
+        if created:
             self.stdout.write(self.style.SUCCESS(f"Superuser {email} créé !"))
         else:
-            self.stdout.write("Superuser existe déjà.")
+            self.stdout.write(self.style.SUCCESS(f"Superuser {email} mis à jour !"))
